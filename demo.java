f13fd95909a867c48fcac091be737d7aa61ad5ee
Dinesh <musewardinesh@gmail.com>
2025-06-10 16:22:26 +0530

    variable 'b' added to code

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ public class FirstClass{
 public static void main(String []args)
 {
     int a = 10; // Third change
+    int b =10;
  System.out.println("First line");
  System.out.println("second line");
 }
