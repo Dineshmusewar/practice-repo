9bcb48994d322d97e9a65419a5fada064eaadfe7
Dineshmusewar <153366647+Dineshmusewar@users.noreply.github.com>
2025-06-10 16:20:04 +0530

    Third line added

--- demo.java
+++ demo.java
@@ -5,5 +5,6 @@ public static void main(String []args)
     int a = 10; // Third change
  System.out.println("First line");
  System.out.println("second line");
+System.out.println("third line"); 
 }
 }
